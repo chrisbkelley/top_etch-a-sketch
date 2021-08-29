f6eb091644b324651f559c5a21eda145dfcd0b8a
chris <81534331+chrisbkelley@users.noreply.github.com>
2021-08-29 18:38:24 -0400

    updates to etch.js and UI layout

--- etch.js
+++ etch.js
@@ -1,15 +1,30 @@
-const maxWidth = 300;
-let gridSquares = 0; 
+//sets width of sketch pad grid
+const gridWidth = 300;
+
+//This multiplier increases the resolution density.  
+const resMultiplier = 16;
+
+//This number should be from 1-10.  
+//It is squared and multiplied by a coefficient to set the resolution of the sketch pad.
+let gridSquares = 1; 
+
+//This is used for resolution increment-btn controls.
+let resCounter = 1;
+
+let selectedColor = 'red';
 
 function makeGridCols(){
-    //creates a grid of divs inside the #container div
     let colCount = Math.sqrt(gridSquares);
         for(i = 0; i < colCount; i++){
-            let container = document.querySelector('#container');
+            let container = document.querySelector('#etch-container');
             let col = document.createElement('div');
             col.setAttribute('class', 'etch-col');
             container.appendChild(col);
         };
+};
+
+function black(){
+
 };
 
 function makeGridSquares (){
@@ -20,11 +35,11 @@ function makeGridSquares (){
             let col = cols[i];
             let square = document.createElement('div');
             square.setAttribute('class', 'etch-square');
-            square.style.width = (maxWidth/gridSquares)*squareCount + 'px';
-            square.style.height = (maxWidth/gridSquares)*squareCount + 'px';
-            square.addEventListener("mouseover", function() {
-                square.style.backgroundColor = "black";
-            });
+            square.style.width = (gridWidth/gridSquares)*squareCount + 'px';
+            square.style.height = (gridWidth/gridSquares)*squareCount + 'px';
+            //square.addEventListener("mouseover", function() {
+            //    this.style.backgroundColor = "black";});
+            square.addEventListener("mouseover", startColor)
             col.append(square);
 
         };
@@ -49,20 +64,84 @@ function removeGrid(){
     };
 };
 
-
-function main(gridSize){
-    const container = document.getElementById('container');
-    container.style.padding = '10px';
-    container.style.width = maxWidth + 'px';
-    container.style.height = maxWidth + 'px';
-
-    if (gridSize > 0 && gridSize <= 100) {
-        gridSquares = gridSize*16;
-        makeGridCols();
-        makeGridSquares();
+function setGridSize(userValue){
+    let gridRes = userValue**2;
+    if (userValue > 0 && userValue <= 10){
+        gridSquares = gridRes*resMultiplier;
     } else {
-        alert('Choose a size in range 1-100.');
-    }
+        console.log('ERROR: Input value out of range')
+    };
+};
+
+function changeGridRes(){
+    removeGrid();
+    main(gridSquares);
+
+};
+
+function incGridRes(){
+    let incTest = resCounter + 1; 
+    if (incTest <=10){
+        gridSquares = incTest;
+        resCounter = resCounter + 1;
+        changeGridRes();
+    } else {
+        resCounter = resCounter;
+    };
+};
+
+function decGridRes(){
+    let incTest = resCounter - 1; 
+    if (incTest > 0){
+        gridSquares = incTest;
+        resCounter = resCounter - 1;
+        changeGridRes();
+    } else {
+        resCounter = resCounter;
+    };
+};
+
+function shadingOn(){
+    const cols = document.querySelectorAll('.etch-col');
+    const squares = document.querySelectorAll('.etch-square');
+    for (i=0; i < cols.length; i++){
+        for (j=0; j < squares.length; j++){
+            squares[i].addEventListener("mouseover", function() {
+                squares[i].style.backgroundColor = 'red';
+            });
+        };
+    };
+};
+
+function changeListenerColors(){
+    squares = document.querySelectorAll('.etch-square')
+    for (i = 0; i < squares.length; i++){
+        squares[i].addEventListener('mouseover', changeColor)
+    };
+};
+
+function startColor(){
+    this.style.backgroundColor = 'black';
+};
+
+function changeColor(){
+    this.style.backgroundColor = selectedColor;
+};
+
+function displayRes(){
+    const display = document.querySelector('#res-display');
+    display.textContent = `${gridSquares}x${gridSquares}`
+};
+
+function main(selectedGridSize){
+    const container = document.getElementById('etch-container');
+    container.style.padding = '10px';
+    container.style.width = gridWidth + 'px';
+    container.style.height = gridWidth + 'px';
+    setGridSize(selectedGridSize);
+    makeGridCols();
+    makeGridSquares();
+    displayRes();
 };
 
 
